18ccb5e0aa065510340bcedd1bb79c06a9256471
Abdul Rehman <rehman79248@gmail.com>
2026-01-30 16:57:29 +0500

    Refactor app.js to implement various date functions

--- app.js
+++ app.js
@@ -1,160 +1,128 @@
-// ========== Q-1 =========== //
+function q1() {
+    var currentTime = new Date();
+    alert(currentTime);
+}
 
-// var currentTime = new Date();
-// alert(currentTime);
+function q2() {
+    var month = new Date();
+    var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var currentMonth = monthArray[month.getMonth()];
+    alert(currentMonth);
+}
 
-// ========== Q-2 =========== //
+function q3() {
+    var date = new Date();
+    var dayArray = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    var currentDay = dayArray[date.getDay()];
+    alert("Today is : " + currentDay);
+}
 
-// var month = new Date();
-// var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+function q4() {
+    var date = new Date();
+    var dayArray = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    var currentDay = dayArray[date.getDay()];
 
-// var currentMonth = monthArray[month.getMonth()]
+    if (currentDay === "Sun" || currentDay === "Sat") {
+        alert("It's Fun Day");
+    } else {
+        alert("It's not a Fun Day");
+    }
+}
 
-// alert(currentMonth);
+function q5() {
+    var date = new Date();
+    var currentDate = date.getDate();
 
-// ========== q-3 ========== //
+    if (currentDate <= 15) {
+        alert("First fifteen days of the month");
+    } else {
+        alert("Last days of the month");
+    }
+}
 
-// var date = new Date();
-// var dayArray = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]
+function q6() {
+    var date = new Date();
+    var timeInMillisecond = date.getTime();
+    var minute = Math.floor(timeInMillisecond / (1000 * 60));
 
-// var currentDay = dayArray[date.getDay()];
+    document.write("Current Date: " + date + "<br>");
+    document.write("Elapsed milliseconds since January 1, 1970: " + timeInMillisecond + "<br>");
+    document.write("Elapsed minutes since January 1, 1970: " + minute);
+}
 
-// alert("Today is : " + currentDay)
+function q7() {
+    var date = new Date();
+    var currentTime = date.getHours();
 
-// ========= Q-4 =========== //
+    if (currentTime < 12) {
+        alert("It's AM");
+    } else {
+        alert("It's PM");
+    }
+}
 
-// var date = new Date();
-// var dayArray = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]
+function q8() {
+    var laterDate = new Date("31 December, 2026");
+    alert(laterDate);
+}
 
-// var currentDay = dayArray[date.getDay()];
+function q9() {
+    var currentTime = new Date().getTime();
+    var ramadanDate = new Date("March 30, 2025").getTime();
+    var dayPassed = Math.floor((currentTime - ramadanDate) / (1000 * 60 * 60 * 24));
 
-// if (currentDay === "Sun" || currentDay === "Sat"){
-//     alert("It's Fun Day")
-// } else {
-//     alert("It's not a Fun Day")
-// }
+    alert(dayPassed + " days have passed since 1st Ramadan, 2025");
+}
 
-// ========== Q-5 =========== //
+function q10() {
+    var datePassed = new Date("1 Jan, 2015");
+    var secondsPassed = Math.floor((new Date().getTime() - datePassed.getTime()) / 1000);
 
-// var date = new Date();
-// var currentDate = date.getDate();
+    document.write("On reference Date " + datePassed + ", <br>" +
+        secondsPassed + " seconds had passed since beginning of 2015");
+}
 
-// if (currentDate <= 15) {
-//     alert("First fifteen days of the month")
-// } else {
-//     alert("Last days of the month")
-// };
+function q11() {
+    var date = new Date();
+    var afterTime = new Date(date);
+    afterTime.setHours(date.getHours() + 1);
 
-// =========== Q-6 ========== //
+    document.write("Current Date : " + date + "<br>");
+    document.write("1 hour later, it will be : " + afterTime);
+}
 
-// var date = new Date();
-// var timeInMillisecond = date.getTime()
-// var convertIntoMinute = timeInMillisecond / (1000 * 60)
-// var minute = Math.floor(convertIntoMinute)
+function q12() {
+    var currentDate = new Date();
+    var pastDate = new Date(currentDate);
+    pastDate.setFullYear(currentDate.getFullYear() - 100);
 
-// document.write("Current Date: " + date + "<br>");
-// document.write("Elapesd millisecond since January 1, 1970: " + timeInMillisecond + "<br>");
-// document.write("Elapesd minutes since January 1, 1970: " + minute + "<br>");
+    document.write("Current Date : " + currentDate + "<br>");
+    document.write("100 Years back, it was : " + pastDate);
+}
 
-// =========== Q-7 ========== //
+function q13() {
+    var age = +prompt("Enter your age?");
+    var birthYear = new Date().getFullYear() - age;
+    alert("Your birth year is : " + birthYear);
+}
 
-// var date = new Date();
-// var currentTime = date.getHours();
+function q14() {
+    var customerName = "ABC Customer";
+    var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var currentMonth = monthArray[new Date().getMonth()];
 
-// if (currentTime <= 12){
-//     alert("It's 'AM'")
-// } else{
-//     alert("It's 'PM'")
-// };
+    var noOfUnits = 410;
+    var chargesPerUnit = 16;
+    var netAmountPayable = noOfUnits * chargesPerUnit;
+    var latePayment = 350;
+    var grossAmount = netAmountPayable + latePayment;
 
-// ========== Q-8 =========== //
-
-// var date = new Date();
-// var laterDate = new Date("31 December, 2026");
-// alert(laterDate);
-
-// ========== Q-9 =========== //
-
-// var date = new Date();
-// var currentTime = date.getTime();
-
-// var ramadanDate = new Date("March 30, 2025");
-// var ramadantime = ramadanDate.getTime();
-
-// var timePassed = currentTime - ramadantime;
-
-// var convertTime = timePassed / (1000 * 60 * 60 * 24);
-// var dayPassed = Math.floor(convertTime);
-
-// alert(dayPassed + " days have passed since 1st Ramadan, 2025");
-
-// ========== Q-10 =========== //
-
-// var date = new Date();
-// var currentTime = date.getTime();
-
-// var datePassed = new Date("1 Jan, 2015");
-// var getTime = datePassed.getTime();
-
-// var remainingTime = currentTime - getTime;
-// var convertIntoSecond = remainingTime / (1000);
-
-// var roundOff = Math.floor(convertIntoSecond);
-
-// document.write("On reference Date " + datePassed + ", <br>" + roundOff + " seconds had passed since begining of 2015")
-
-// ========== Q-11 =========== //
-
-// var date = new Date();
-// var currentHours = date.getHours()
-
-// var afterTime = new Date(date);
-// afterTime.setHours(currentHours + 1);
-
-// document.write("Current Date : " + date + "<br>");
-// document.write("1 hour ago, It was : " + afterTime);
-
-// ========== Q-12 =========== //
-
-// var currentDate = new Date();
-
-// var pastDate = new Date(currentDate)
-// pastDate.setFullYear(currentDate.getFullYear() - 100)
-
-// document.write("Current Date : " + currentDate + "<br>")
-// document.write("100 Years back, it was : " + pastDate)
-
-// ========== Q-13 =========== //
-
-// var birthYear = +prompt("Enter your age?")
-
-// var currentTime = new Date();
-// var getFullYear = currentTime.getFullYear();
-
-// var birthYear = getFullYear - birthYear
-
-// alert("your birth year is : " + birthYear)
-  
-// ========== Q-14 =========== //
-
-// var customerName = "ABC Customer"
-// var month = new Date();
-// var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
-// var currentMonth = monthArray[month.getMonth()]
-
-// var noOfUnits = 410;
-// var chargesPerUnit = 16;
-// var netAmountPayable = noOfUnits * chargesPerUnit;
-// var latePayment = 350;
-// var grossAmount = netAmountPayable + latePayment;
-
-// document.write("<h1>K-Elelctric Bill</h1> <br>")
-// document.write("Customer Name: <b>" + customerName + "</b><br>")
-// document.write("Current Month: <b>" + currentMonth + "</b><br>")
-// document.write("Number of units: <b>" + noOfUnits + "</b><br>")
-// document.write("Charges per unit: <b>" + chargesPerUnit + "</b><br><br>")
-
-// document.write("Net Amount Payable (within Due Date): <b>" + netAmountPayable + "</b><br>")
-// document.write("Late Payment Surcharge: <b>" + latePayment + "</b><br>")
-// document.write("Gross Amount Payable (after Due Date): <b>" + grossAmount + "</b><br>")
+    document.write("<h1>K-Electric Bill</h1>");
+    document.write("Customer Name: <b>" + customerName + "</b><br>");
+    document.write("Current Month: <b>" + currentMonth + "</b><br>");
+    document.write("Number of Units: <b>" + noOfUnits + "</b><br>");
+    document.write("Charges per Unit: <b>" + chargesPerUnit + "</b><br><br>");
+    document.write("Net Amount Payable: <b>" + netAmountPayable + "</b><br>");
+    document.write("Late Payment Surcharge: <b>" + latePayment + "</b><br>");
+    document.write("Gross Amount Payable: <b>" + grossAmount + "</b>");
+}
